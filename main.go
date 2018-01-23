394b1ac36e029801030c24b9ea415afe9e545e4b
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-23 14:33:35 -0200

    Updated routes

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func main() {
 	Servidor := new(controllers.ServidorController)
 
 	router.HandleFunc("/api/servidores", Servidor.GetServidor).Methods("GET")
+	router.HandleFunc("/api/servidor/", Servidor.PostServidor).Methods("POST")
 	router.HandleFunc("/api/servidor/{matricula:[0-9]+}", Servidor.GetServidorMat).Methods("GET") // URL parameter with Regex in URL
-	// router.HandleFunc("/api/servidor/}", Servidor.PostServidor).Methods("POST")                   // URL parameter with Regex in URL
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
