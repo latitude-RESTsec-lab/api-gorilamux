fd4681b879d43ff5c739b2b71f82f71bdcc6b308
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-23 18:57:20 -0200

    corrected time hash changed var names

--- controllers/pessoal.go
+++ controllers/pessoal.go
@@ -19,14 +19,14 @@ type ErrorBody struct {
 }
 
 type Servidor struct {
-	ID                 int    `db:"id, primarykey, autoincrement" json:"id"`
-	Siape              int    `db:"siape" json:"siape"`
-	Id_pessoa          int    `db:"id_pessoa" json:"id_pessoa"`
-	Nome               string `db:"nome" json:"nome"`
-	Matricula_interna  int    `db:"matricula_interna" json:"matricula_interna"`
-	Nome_identificacao string `db:"nome_identificacao" json:"nome_identificacao"`
-	Data_nascimento    string `db:"data_nascimento" json:"data_nascimento"`
-	Sexo               string `db:"sexo" json:"sexo"`
+	ID                int    `db:"id, primarykey, autoincrement" json:"id"`
+	Siape             int    `db:"siape" json:"siape"`
+	Idpessoa          int    `db:"id_pessoa" json:"id_pessoa"`
+	Nome              string `db:"nome" json:"nome"`
+	Matriculainterna  int    `db:"matricula_interna" json:"matricula_interna"`
+	Nomeidentificacao string `db:"nome_identificacao" json:"nome_identificacao"`
+	Datanascimento    string `db:"data_nascimento" json:"data_nascimento"`
+	Sexo              string `db:"sexo" json:"sexo"`
 }
 
 type ServidorController struct{}
@@ -49,10 +49,10 @@ func (ctrl ServidorController) GetServidor(w http.ResponseWriter, r *http.Reques
 
 	var pessoas []Servidor
 
-	var id, id_pessoa, matricula_interna, siape int
-	var nome, nome_identificacao, data_nascimento, sexo string
+	var id, idpessoa, matriculainterna, siape int
+	var nome, nomeidentificacao, datanascimento, sexo string
 	for rows.Next() {
-		err := rows.Scan(&id, &siape, &id_pessoa, &matricula_interna, &nome_identificacao, &nome, &data_nascimento, &sexo)
+		err := rows.Scan(&id, &siape, &idpessoa, &matriculainterna, &nomeidentificacao, &nome, &datanascimento, &sexo)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(400)
@@ -63,14 +63,14 @@ func (ctrl ServidorController) GetServidor(w http.ResponseWriter, r *http.Reques
 		}
 
 		pessoas = append(pessoas, Servidor{
-			ID:                 id,
-			Siape:              siape,
-			Id_pessoa:          id_pessoa,
-			Nome:               nome,
-			Matricula_interna:  matricula_interna,
-			Nome_identificacao: nome_identificacao,
-			Data_nascimento:    data_nascimento,
-			Sexo:               sexo,
+			ID:                id,
+			Siape:             siape,
+			Idpessoa:          idpessoa,
+			Nome:              nome,
+			Matriculainterna:  matriculainterna,
+			Nomeidentificacao: nomeidentificacao,
+			Datanascimento:    datanascimento,
+			Sexo:              sexo,
 		})
 	}
 	if err := rows.Err(); err != nil {
@@ -118,10 +118,10 @@ func (ctrl ServidorController) GetServidorMat(w http.ResponseWriter, r *http.Req
 
 	var pessoas []Servidor
 
-	var id, id_pessoa, matricula_interna, siape int
-	var nome, nome_identificacao, data_nascimento, sexo string
+	var id, idpessoa, matriculainterna, siape int
+	var nome, nomeidentificacao, datanascimento, sexo string
 	for rows.Next() {
-		err := rows.Scan(&id, &siape, &id_pessoa, &matricula_interna, &nome_identificacao, &nome, &data_nascimento, &sexo)
+		err := rows.Scan(&id, &siape, &idpessoa, &matriculainterna, &nomeidentificacao, &nome, &datanascimento, &sexo)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(400)
@@ -132,14 +132,14 @@ func (ctrl ServidorController) GetServidorMat(w http.ResponseWriter, r *http.Req
 		}
 
 		pessoas = append(pessoas, Servidor{
-			ID:                 id,
-			Siape:              siape,
-			Id_pessoa:          id_pessoa,
-			Nome:               nome,
-			Matricula_interna:  matricula_interna,
-			Nome_identificacao: nome_identificacao,
-			Data_nascimento:    data_nascimento,
-			Sexo:               sexo,
+			ID:                id,
+			Siape:             siape,
+			Idpessoa:          idpessoa,
+			Nome:              nome,
+			Matriculainterna:  matriculainterna,
+			Nomeidentificacao: nomeidentificacao,
+			Datanascimento:    datanascimento,
+			Sexo:              sexo,
 		})
 	}
 	if err := rows.Err(); err != nil {
@@ -182,7 +182,7 @@ func (ctrl ServidorController) PostServidor(w http.ResponseWriter, r *http.Reque
 
 	// REGEX CHEKING PHASE
 	regex, _ := regexp.Compile(`^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)T([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])Z$`)
-	if !regex.MatchString(ser.Data_nascimento) {
+	if !regex.MatchString(ser.Datanascimento) {
 		regexcheck = true
 		Reasons = append(Reasons, ErrorBody{
 			Reason: "[data_nascimento] failed to match API requirements. It should look like this: 1969-02-12T00:00:00Z",
@@ -196,7 +196,7 @@ func (ctrl ServidorController) PostServidor(w http.ResponseWriter, r *http.Reque
 		})
 	}
 	regex, _ = regexp.Compile(`^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$`)
-	if !regex.MatchString(ser.Nome_identificacao) {
+	if !regex.MatchString(ser.Nomeidentificacao) {
 		regexcheck = true
 		Reasons = append(Reasons, ErrorBody{
 			Reason: "[nome_identificacao] failed to match API requirements. It should look like this: Firstname Middlename(optional) Lastname",
@@ -209,40 +209,23 @@ func (ctrl ServidorController) PostServidor(w http.ResponseWriter, r *http.Reque
 			Reason: "[sexo] failed to match API requirements. It should look like this: M for male, F for female",
 		})
 	}
-	// regex, _ = regexp.Compile(`\b[0-9]+\b`)
-	// if !regex.MatchString(strconv.Itoa(ser.Siape)) {
-	// 	regexcheck = true
-	// 	Reasons = append(Reasons, ErrorBody{
-	// 		error_reason: "[siape] failed to match API requirements. It should be only numeric.",
-	// 	})
-	// }
-	// regex, _ = regexp.Compile(`\b[0-9]+\b`)
-	// if !regex.MatchString(strconv.Itoa(ser.Id_pessoa)) {
-	// 	regexcheck = true
-	// 	Reasons = append(Reasons, ErrorBody{
-	// 		error_reason: "[id_pessoa] failed to match API requirements. It should be only numeric.",
-	// 	})
-	// }
 	if regexcheck {
 		w.WriteHeader(400)
 		json.NewEncoder(w).Encode(&Reasons)
 		return
 	}
 	// END OF REGEX CHEKING PHASE
-	timestamp := time.Now().Unix()
+	timestamp := time.Now().UTC().Format("2006-01-02T15:04:05-0700")
 	b := md5.Sum([]byte(fmt.Sprintf(string(ser.Nome), string(timestamp))))
 	bid := binary.BigEndian.Uint64(b[:])
-	// log.Println(strconv.Atoi(string(b[:])))
-	// ser.Data_nascimento = serTex.Data_nascimento
-	// ser.ID, _ = strconv.Atoi(serTex.ID)
 
 	q := fmt.Sprintf(`
 		INSERT INTO rh.servidor_tmp(
 			nome, nome_identificacao, siape, id_pessoa, matricula_interna, id_foto,
 			data_nascimento, sexo)
 			VALUES ('%s', '%s', %d, %d, %d, null, '%s', '%s');
-			`, ser.Nome, ser.Nome_identificacao, ser.Siape, ser.Id_pessoa, bid%99999,
-		ser.Data_nascimento, ser.Sexo) //String formating
+			`, ser.Nome, ser.Nomeidentificacao, ser.Siape, ser.Idpessoa, bid%9999999,
+		ser.Datanascimento, ser.Sexo) //String formating
 
 	rows, err := db.GetDB().Query(q)
 	if err != nil {
