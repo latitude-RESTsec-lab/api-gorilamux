62a1b0e09c4e307de14fe3b522e7a84e2a09e72d
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-30 19:33:50 -0200

    HTTPS and HTTP rerouting

--- main.go
+++ main.go
@@ -12,15 +12,29 @@ import (
 )
 
 func main() {
-	router := mux.NewRouter()
 
 	db.Init()
 	defer db.GetDB().Db.Close()
 	Servidor := new(controllers.ServidorController)
+	httpsRouter := mux.NewRouter()
 
-	router.HandleFunc("/api/servidores", Servidor.GetServidor).Methods("GET")
-	router.HandleFunc("/api/servidor/", Servidor.PostServidor).Methods("POST")
-	router.HandleFunc("/api/servidor/{matricula:[0-9]+}", Servidor.GetServidorMat).Methods("GET") // URL parameter with Regex in URL
+	httpsRouter.HandleFunc("/api/servidores", Servidor.GetServidor).Methods("GET")
+	httpsRouter.HandleFunc("/api/servidor/", Servidor.PostServidor).Methods("POST")
+	httpsRouter.HandleFunc("/api/servidor/{matricula:[0-9]+}", Servidor.GetServidorMat).Methods("GET") // URL parameter with Regex in URL
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	httpRouter := mux.NewRouter()
+	httpsRouter.HandleFunc("/api/servidores", func(w http.ResponseWriter, r *http.Request) {
+		target := "https://" + r.Host + r.URL.Path
+		if len(r.URL.RawQuery) > 0 {
+			target += "?" + r.URL.RawQuery
+		}
+		log.Printf("redirect to: %s", target)
+		http.Redirect(w, r, target,
+			http.StatusTemporaryRedirect)
+	}).Methods("GET")
+	httpsRouter.HandleFunc("/api/servidor/{matricula:[0-9]+}", Servidor.GetServidorMat).Methods("GET") // URL parameter with Regex in URL
+	// http.DefaultTransport.RoundTrip("http2")
+	go http.ListenAndServe(":80", httpRouter)
+	log.Fatal(http.ListenAndServeTLS(":443", "./devssl/server.pem", "./devssl/server.key", httpsRouter))
+
 }
