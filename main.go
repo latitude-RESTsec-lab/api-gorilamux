2566d8545e6b009843d9703d41cd5ed734cf96b0
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-31 01:35:40 -0200

    removed http routes

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting GORILLA MUX API")
 	file, fileErr := os.Create("server.log")
 	if fileErr != nil {
 		fmt.Println(fileErr)
@@ -23,26 +24,19 @@ func main() {
 
 	db.Init()
 	defer db.GetDB().Db.Close()
+
 	Servidor := new(controllers.ServidorController)
+
 	httpsRouter := mux.NewRouter()
 
 	httpsRouter.HandleFunc("/api/servidores", Servidor.GetServidor).Methods("GET")
 	httpsRouter.HandleFunc("/api/servidor/", Servidor.PostServidor).Methods("POST")
 	httpsRouter.HandleFunc("/api/servidor/{matricula:[0-9]+}", Servidor.GetServidorMat).Methods("GET") // URL parameter with Regex in URL
-
-	httpRouter := mux.NewRouter()
-	httpsRouter.HandleFunc("/api/servidores", func(w http.ResponseWriter, r *http.Request) {
-		target := "https://" + r.Host + r.URL.Path
-		if len(r.URL.RawQuery) > 0 {
-			target += "?" + r.URL.RawQuery
-		}
-		log.Printf("redirect to: %s", target)
-		http.Redirect(w, r, target,
-			http.StatusTemporaryRedirect)
-	}).Methods("GET")
-	httpsRouter.HandleFunc("/api/servidor/{matricula:[0-9]+}", Servidor.GetServidorMat).Methods("GET") // URL parameter with Regex in URL
-	// http.DefaultTransport.RoundTrip("http2")
-	go http.ListenAndServe(":80", httpRouter)
-	log.Fatal(http.ListenAndServeTLS(":443", "./devssl/server.pem", "./devssl/server.key", httpsRouter))
+	err := http.ListenAndServeTLS(":443", "./devssl/server.pem", "./devssl/server.key", httpsRouter)
+	if err != nil {
+		fmt.Println(err.Error())
+		log.Fatal(err)
+		return
+	}
 
 }
